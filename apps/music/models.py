2faa491540181d25be0d76800d5094344a08ddda
MrSinaMaleki <sina67631@gmail.com>
2024-10-21 09:51:25 +0330

    Basic music model + str representation

--- apps/music/models.py
+++ apps/music/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Music(models.Model):
+    title = models.CharField(max_length=255)
+    singer = models.CharField(max_length=128)
+    code = models.CharField(max_length=128, unique=True)
+    img = models.ImageField(upload_to='music')
+
+    def __str__(self):
+        return f"{self.code}. {self.singer} --> {self.title}"
